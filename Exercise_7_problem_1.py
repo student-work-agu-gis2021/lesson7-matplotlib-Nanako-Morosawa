6d78368994b321c98ad623ce6786e55eacb80ebd
NanakoMorosawa <>
2021-05-30 15:13:06 +0000

    I set the type of the plot using parameter kind=scatter.

--- Exercise_7_problem_1.py
+++ Exercise_7_problem_1.py
@@ -58,15 +58,14 @@ assert len(colors) == 1000, "There should be 1000 random numbers for colors"
 # Plot a scatter plot
 # YOUR CODE HERE 3
 import matplotlib.pyplot as plt
-plt.scatter(x,y,s=50,c='black',cmap='rainbow',edgecolor='red')
+plt.scatter(x,y,s=50,c=colors,cmap='jet',edgecolor='black')
 
 # Add labels and title
+# YOUR CODE HERE 4
 plt.title("My random candy points")
 plt.xlabel("X-labels")
 plt.ylabel("Y-labels")
 plt.show()
-# YOUR CODE HERE 4
-
 # Save the plot as a png file:
 outputfp = "my_first_plot.png"
 
